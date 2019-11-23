c6296cde094d86f558f3da81d26a815eae7db276
Todd N Townsend <Todd N Townsend@DESKTOP-K6T4SRJ>
2019-11-23 14:11:37 -0800

    Added Dummy print functions

--- cache_operation.c
+++ cache_operation.c
@@ -15,3 +15,48 @@ int WhichWay(int set)
 {
 	return 1;
 }
+
+void readData(void)
+{
+	printf("operation READ_DATA");
+}
+
+void writeData(void)
+{
+	printf("operation WRITE_DATA");
+}
+
+void ReadInstruction(void)
+{
+	printf("operation READ_INSTRUCTION");
+}
+
+void SnoopedInvalidate(void)
+{
+	printf("operation SNOOPED_INVALIDATE");
+}
+
+void SnoopedRead(void)
+{
+	printf("operation SNOOPED_READ");
+}
+
+void SnoopedWrite(void)
+{
+	printf("operation SNOOPED_WRITE");
+}
+
+void SnoopedReadX(void)
+{
+	printf("operation SNOOPED_READ_X");
+}
+
+void ClearAndSet(void)
+{
+	printf("operation CLEAR_AND_RESET");
+}
+
+void PrintCacheLine(void)
+{
+	printf("operation PRINT_CACHE_LINE");
+}
--- cache_operation.h
+++ cache_operation.h
@@ -7,8 +7,18 @@
 
 #ifndef CACHE_OPERATION_H_
 #define CACHE_OPERATION_H_
-
+#include <stdio.h>
 
 void UpdatePLRU(int set, int w);
 int WhichWay(int set);
+void readData(void);
+void writeData(void);
+void ReadInstruction(void);
+void SnoopedInvalidate(void);
+void SnoopedRead(void);
+void SnoopedWrite(void);
+void SnoopedReadX(void);
+void ClearAndSet(void);
+void PrintCacheLine(void);
+
 #endif /* CACHE_OPERATION_H_ */
